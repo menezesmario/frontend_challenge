ae96aa718fddffe91bc0a2bf09f5bb12fc931ee2
Mário Menezes <mario082@gmail.com>
2021-04-13 18:25:02 -0300

    first ok dellivery

--- script.js
+++ script.js
@@ -47,69 +47,7 @@ console.log(moreProducts)
 
 
 
-// page++ 
 
-// const req2 = fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`)
-// .then(response => response.json())
-// .then(jsonBody => {
-//     const items = jsonBody.products;
-
-//     addProductsIntoDOM = () => {
-//         let productsContainer = document.getElementById('products');
-//         items.map((data) => {
-//             productsContainer.innerHTML+= `                                
-//                 <div class="product" key=`+data.id+` >
-//                 <div class="img-container">
-//                     <img src="`+data.image+`" />
-//                 </div>
-//                     <p class="product-name">`+data.name+`</p>
-//                     <p class="product-description">`+data.description+`</p>
-//                     <p class="old-price">De: R$`+data.oldPrice+`,00</p>
-//                     <p class="price">Por: R$`+data.price+`,00</p>
-//                     <p class="instalments">ou `+data.installments.count+` x `+data.installments.value+`0</p>
-//                     <button class="btn-buy">Comprar</button>
-//                 </div>                
-            
-//             `
-//         })
-//     }
-    
-//     addProductsIntoDOM();
-
-// });
-
-
-
-
-   
-
-    // const getNextProducts = () => {
-    //     page++
-    //     addProductsIntoDOM()
-    // }
-
-    // const removeLoader = () => {
-    //     setTimeout(() => {
-    //         loaderContainer.classList.remove('show')
-    //         getNextProducts()
-    //     }, 1000);
-    // }
-
-    // const showLoader = () => {
-    //     loaderContainer.classList.add('show')
-    //     removeLoader()
-    // }
-
-    // const clickButton = document.getElementsByClassName("more-products")
-    // console.log(clickButton)
-
-    // clickButton.addEventListener("click", () => {
-    //     getNextProducts();
-    // });  
-
-
-
-    
 
 
 
